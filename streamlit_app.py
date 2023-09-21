735712940f7526a2b13409801c9e3361244e88a5
TOSHIYA TANIMOTO <tanimoto.toshiya@jgc.com>
2023-09-21 23:11:32 +0900

    EEE-3rd

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,26 @@ st.title("Homepage for My ChatGPT")
 st.header('Hello! This page offers multiple apps that have added specific features to ChatGPT.')
 
 st.markdown('''
-Please select an app from the slidebar. 
+Please select an app from the sidebar. 
 
 Below are descriptions for each app.
-#ask CSV
-    - You can ask questions about the data from the uploaded CSV file!
-#ask PDF 
-    - You can ask questions about the content of the uploaded PDF! 
-#SimilarityCalculator
-    - From the uploaded CSV file dataset, it will extracted and display the top 5 items most relevant to the input text!
-#WebSearch
-    - When you ask a question, it will provide answers using information from the web!
+
+- **AAA: ask PDF**
+  You can ask questions about the content of the uploaded PDF! 
+            
+- **BBB: ask CSV**
+  You can ask questions about the data from the uploaded CSV file!
+
+- **CCC: Similarity Calculator**
+  From the uploaded CSV file dataset, it will extract and display the top 5 items most relevant to the input text!
+
+- **DDD: Web Search**
+  When you ask a question, it will provide answers using information from the web!
+            
+- **EEE: Python Agent**
+  It will respond to your questions using Python!
 ''')
+
 
 
 # *** sidebar
