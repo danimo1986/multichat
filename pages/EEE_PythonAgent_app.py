74bf7adb6c216f8943fa19f260376f3209a997ff
TOSHIYA TANIMOTO <tanimoto.toshiya@jgc.com>
2023-09-21 22:56:42 +0900

    EEE-2nd

--- pages/EEE_PythonAgent_app.py
+++ pages/EEE_PythonAgent_app.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+import sys
 from langchain.llms import OpenAI
 from langchain.agents.agent_toolkits import create_python_agent
 from langchain.tools.python.tool import PythonREPLTool
@@ -30,7 +31,7 @@ def main():
     agent_executor = create_python_agent(
         llm=llm,
         tool=PythonREPLTool(),
-        verbose=True,
+        verbose=True,  # デバッグ情報を表示するためにverboseをTrueに設定
     )
 
     # User input text box
@@ -38,25 +39,19 @@ def main():
 
     if st.button("Submit"):
         if user_input.strip() != "":
+            # Capture stdout to display debug information
+            sys.stdout = st
+
             # Get the response from the chatbot
             response = agent_executor.run(user_input)
 
+            # Restore the original stdout
+            sys.stdout = sys.__stdout__
+
             # Display the chatbot's response
             st.write("Chatbot Response:")
-            st.write(f"Chatbot: {response}")
-
-            # Check if the response is a dictionary
-            if isinstance(response, dict):
-                # Execute and display Python code
-                if response.get("action") == "Python_REPL" and response.get("action_input"):
-                    st.code(response["action_input"], language="python")
-                    st.write("Chatbot Observations:")
-                    for observation in response.get("observations", []):
-                        st.write(observation)
-                    st.write("Final Answer:")
-                    st.write(response.get("final_answer"))
-                else:
-                    st.write("Response is not Python code.")
+            st.markdown(f"<p style='font-size:24px; color:red;'>Chatbot: {response}</p>", unsafe_allow_html=True)
+            #st.write(f"Chatbot: {response}")
 
 if __name__ == "__main__":
     main()
