4d11cd08ed102e9bb6c68f3ea799bd1542ee108c
TOSHIYA TANIMOTO <tanimoto.toshiya@jgc.com>
2023-09-21 18:36:25 +0900

    11th

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,21 @@ import streamlit as st
 
 
 st.title("Homepage for My ChatGPT")
-st.header('These apps provide multiple functions for ChatGPT')
+st.header('Hello! This page offers multiple apps that have added specific features to ChatGPT.')
 
-#st.markdown('''
-#Hello!
-#
-#The following function will be added：
-#- XX
-#- XX
-#''')
+st.markdown('''
+Please select an app from the slidebar. 
+
+Below are descriptions for each app.
+#ask CSV
+    - You can ask questions about the data from the uploaded CSV file!
+#ask PDF 
+    - You can ask questions about the content of the uploaded PDF! 
+#SimilarityCalculator
+    - From the uploaded CSV file dataset, it will extracted and display the top 5 items most relevant to the input text!
+#WebSearch
+    - When you ask a question, it will provide answers using information from the web!
+''')
 
 
 # *** sidebar
